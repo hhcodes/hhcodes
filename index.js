bc1584219706d437d37310936e603596ee149db5
hhcodes <huhaibj@163.com>
2017-09-26 11:37:53 +0800

    版本二

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
    	var scrollPos;  
    	var scrollOld = 0;
-	var n = document.documentElement.clientHeight;
+    var n = document.documentElement.clientHeight;
+	var w = document.documentElement.offsetWidth;
+    var s = document.getElementsByClassName('bl');
+window.onload  = function  () {
+    if(w>1000){
+        for (var i = s.length - 1; i >= 0; i--) {
+            s[i].style.height = n-130+"px";
+            s[i].style.width = "100%";
+            s[i].style.paddingTop = "130px";
+        }
+    }    
+}
 window.onscroll = function () {  
     if (window.pageYOffset) {  
     scrollPos = window.pageYOffset; }  
